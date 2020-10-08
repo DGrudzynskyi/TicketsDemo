d12d418b5f34f83c0d45802c974435c51538ac84
Danylo Zinchenko <zindan466@gmail.com>
2020-10-08 18:45:12 +0300

    Implimented BookingAgency and BookingService classes

---
+++ TicketsDemo.Data/Entities/BookingServiceAggregate/BookingAgency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketsDemo.Data.Entities
+{
+    public class BookingAgency
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double FareCoef { get; set; }
+        public List<BookingService> BookingServices { get; set; }
+    }
+}
