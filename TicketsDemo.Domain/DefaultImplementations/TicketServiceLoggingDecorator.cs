a09e4e716d6b112da077331e2971d4d763796b74
Melpops <sirenko73657@gmail.com>
2020-10-27 22:53:53 +0200

    WTF I do the same thing twice in loggingdecorator

--- TicketsDemo.Domain/DefaultImplementations/TicketServiceLoggingDecorator.cs
+++ TicketsDemo.Domain/DefaultImplementations/TicketServiceLoggingDecorator.cs
@@ -13,53 +13,31 @@ namespace TicketsDemo.Domain.DefaultImplementations
     abstract class TicketServiceLoggingDecorator : ITicketService
     {
         private ITicketService _decoratedobject;
-        string choice = "F";
-        string file_path = @"D:\Tikitake true\log.txt";
-        public TicketServiceLoggingDecorator(ITicketService service)
+        private ILogger _logger;
+        public TicketServiceLoggingDecorator(ITicketService service, FileLogger logger)
         {
             _decoratedobject = service;
+            _logger = logger;
         }
 
         public Ticket CreateTicket(int reservationId, string firstName, string lastName)
         {
-            switch (choice)
+            StringBuilder sb = new StringBuilder($"\t\t\tAttempt to create ticket with {reservationId}, for {firstName}-{lastName}\n");
+            try
             {
-                case "D":
-                    Debug.WriteLine($"attempt to create ticket with {reservationId}, for {firstName}-{lastName}");
-                    try
-                    {
-                        var decorTicket = _decoratedobject.CreateTicket(reservationId, firstName, lastName);
-                        Debug.WriteLine($"Ticket created succesfully for {reservationId} with passenger {firstName}-{lastName}");
-                        return decorTicket;
-                    }
-                    catch (Exception ex)
-                    {
-                        Debug.WriteLine($"{ex.Message} \n Error due to create Ticket for Reservation: {reservationId} with passenger {firstName}-{lastName}");
-                        throw ex;
-                    }
-                case "F":
-                    using (FileStream fstream = new FileStream($"{file_path}", FileMode.OpenOrCreate))
-                    {
-                        string result = "";
-                        try
-                        {
-                            var decorTicket = _decoratedobject.CreateTicket(reservationId, firstName, lastName);
-                            result += ($"Ticket created succesfully for {reservationId} with passenger {firstName}-{lastName}");
-
-                            byte[] array = System.Text.Encoding.Default.GetBytes(result);
-                            fstream.Write(array, 0, array.Length);
-                            return decorTicket;
-                        }
-                        catch (Exception ex)
-                        {
-                            result += ($"{ex.Message} \n Error due to create Ticket for Reservation: {reservationId} with passenger {firstName}-{lastName}");
-                            byte[] array = System.Text.Encoding.Default.GetBytes(result);
-                            fstream.Write(array, 0, array.Length);
-                            throw ex;
-                        }
-
-                    }
-            }
+                var decorTicket = _decoratedobject.CreateTicket(reservationId, firstName, lastName);
+                sb.Append($"Ticket created succesfully for {reservationId} with passenger {firstName}-{lastName}");
+                _logger.Log(sb.ToString());
+                return decorTicket;
+             }
+            catch (Exception ex)
+            {
+                 sb.Append($"{ex.Message} \n Error due to create Ticket for Reservation: {reservationId} with passenger {firstName}-{lastName}");
+                _logger.Log(sb.ToString());
+                throw ex;
+             }
+                
+            
         }
 
         public void SellTicket(Ticket ticket)
