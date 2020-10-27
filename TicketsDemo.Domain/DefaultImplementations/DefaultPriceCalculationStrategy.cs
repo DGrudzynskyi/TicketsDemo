258bc4497f0389d43044be95cdd086a84526b29a
kliuha <vavelog@gmail.com>
2020-10-27 10:02:24 +0200

    tasks

---
+++ TicketsDemo.Domain/DefaultImplementations/DefaultPriceCalculationStrategy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketsDemo.Data.Entities;
+using TicketsDemo.Data.Repositories;
+using TicketsDemo.Domain.Interfaces;
+
+namespace TicketsDemo.Domain.DefaultImplementations.PriceCalculationStrategy
+{
+    public class DefaultPriceCalculationStrategy : IPriceCalculationStrategy
+    {
+        private IRunRepository _runRepository;
+        private ITrainRepository _trainRepository;
+
+        public DefaultPriceCalculationStrategy(IRunRepository runRepository, ITrainRepository trainRepository) {
+            _runRepository = runRepository;
+            _trainRepository = trainRepository;
+        }
+
+        public List<PriceComponent> CalculatePrice(PlaceInRun placeInRun)
+        {
+            var components = new List<PriceComponent>();
+
+            var run = _runRepository.GetRunDetails(placeInRun.RunId);
+            var train = _trainRepository.GetTrainDetails(run.TrainId);
+            var place = 
+                train.Carriages
+                    .Select(car => car.Places.SingleOrDefault(pl => 
+                        pl.Number == placeInRun.Number && 
+                        car.Number == placeInRun.CarriageNumber))
+                    .SingleOrDefault(x => x != null);
+
+            var placeComponent = new PriceComponent() { Name = "Price" };
+            placeComponent.Value = place.Carriage.DefaultPrice * place.PriceMultiplier;
+            components.Add(placeComponent);
+
+
+           
+
+            return components;
+        }
+    }
+}
