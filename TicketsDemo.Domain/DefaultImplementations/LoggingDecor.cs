c80bd59ee016d6a98fdeafc7aa2504c7a9f925e7
Kirill Nadelyaev <k.nadelyaev17@gmail.com>
2020-11-03 20:25:32 +0200

    decorator

---
+++ TicketsDemo.Domain/DefaultImplementations/LoggingDecor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using TicketsDemo.Data.Entities;
+using TicketsDemo.Domain.Interfaces;
+
+namespace TicketsDemo.Domain.DefaultImplementations
+{
+    public class LoggingDecor : IReservationService
+    {
+        private IReservationService _decorObject;
+        private ILogger _logger;
+        private string _path;
+
+        public LoggingDecor(IReservationService reservation, ILogger logger)
+        {
+            _decorObject = reservation;
+            _logger = logger;
+            _path = $"{AppDomain.CurrentDomain.BaseDirectory}log.txt"; 
+        }
+
+        public bool IsActive(Reservation reservation)
+        {
+            return _decorObject.IsActive(reservation);
+        }
+
+        public bool PlaceIsOccupied(PlaceInRun place)
+        {
+            return _decorObject.PlaceIsOccupied(place);
+        }
+
+        public void RemoveReservation(Reservation reservation)
+        {
+            
+        }
+
+        public Reservation Reserve(PlaceInRun place)
+        {
+            try
+            {
+                var reserv = _decorObject.Reserve(place);
+                var message = $"Reservation is successful\nPlace: {place.Id}, Train: {place.Run.TrainId}, Departure: {place.Run.Date}";
+
+                _logger.Log(message, LogSeverity.Info);
+                return reserv;
+            }
+            catch (Exception ex)
+            {
+                var errorMassage = $"Reservation is unsuccessful\nPlace: {place.Id}, Train: {place.Run.TrainId}, Departure: {place.Run.Date}\n{ex.Message}";
+
+
+                _logger.Log(errorMassage, LogSeverity.Error);
+                throw ex;
+            }
+        }
+    }
+}
