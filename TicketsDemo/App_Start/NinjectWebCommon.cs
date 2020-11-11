1f4e01025c83e5ec657651fb12847ba0277b3f75
Дима Резниченко <dmitrorezn@gmail.com>
2020-11-11 11:17:52 +0200

    fixed binde

--- TicketsDemo/App_Start/NinjectWebCommon.cs
+++ TicketsDemo/App_Start/NinjectWebCommon.cs
@@ -80,7 +80,15 @@ namespace TicketsDemo.App_Start
 
             kernel.Bind<IReservationService>().To<ReservationService>();
             //todo factory
-            kernel.Bind<IPriceCalculationStrategy>().To<DefaultPriceCalculationStrategy>();
+            //kernel.Bind<IPriceCalculationStrategy>().To<DefaultPriceCalculationStrategy>();
+            kernel.Bind<IPriceCalculationStrategy>().ToMethod<NewPriceCalculationStrategy>(ctx =>
+            {
+                return new NewPriceCalculationStrategy(new System.Collections.Generic.List<IPriceCalculationStrategy>() {
+                    ctx.Kernel.Get<DefaultPriceCalculationStrategy>(),
+                    ctx.Kernel.Get<TeaCoffeBedLincePriceStrategy>()
+                });
+
+            });
             kernel.Bind<ILogger>().ToMethod(x =>
                 new FileLogger(HttpContext.Current.Server.MapPath("~/App_Data")));
         }        
